3ef4b7180be060091d526490601c498030e4af33
Elthon Jhon Kevin <elthonjhonkevin@gmail.com>
2026-02-01 22:05:12 +0700

    revision again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from '@/app/admin/layout/AuthProvider';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    default: 'UMKM Desa Marga Kaya',
+    template: '%s | UMKM Desa Marga Kaya',
+  },
+  description: 'Katalog UMKM Desa Marga Kaya.',
+  icons: {
+    icon: '/logo.png',
+  },
+};
 
 export default function RootLayout({
   children,
